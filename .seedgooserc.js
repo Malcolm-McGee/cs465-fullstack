6ac18ac7e610bf2c825a7ffb7e2ad4ee73477699
Malcolm McGee <malcolm.mcgee@snhu.edu>
2023-12-01 18:35:53 -0600

    Module 5

--- .seedgooserc.js
+++ .seedgooserc.js
@@ -1,6 +1,6 @@
 // .seedgooserc.js
 module.exports = {
-    modelBaseDirectory: 'app_server/models', //model diretory name
+    modelBaseDirectory: 'app_api/models', //model diretory name
     models: ['*.js', '!db.js'], //model matcher
     data: 'data', //data directory name
     db: 'mongodb://localhost:27017/travlr' // db connection url
